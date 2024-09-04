14d5dda24246df1c15612a97b395953639ed11e4
digiT000 <fareldeksano000@gmail.com>
2024-09-04 10:04:51 +0700

    Update setelah pembahasan, add kak bagas solution

--- day_06.js
+++ day_06.js
@@ -11,7 +11,7 @@ class Students {
     let date = new Date();
     let ageMs = new Date(this.dateYear);
 
-    return Math.floor((date - ageMs) / (1000 * 60 * 60 * 24 * 365));
+    return ((date - ageMs) / (1000 * 60 * 60 * 24 * 365)).toFixed();
   }
 }
 
@@ -19,20 +19,20 @@ const listStudent = [
   new Students(
     "Farel Darari Deksano",
     "farledeksano000@gmail.com",
-    "06-27-1994",
-    50
+    "05-20-2000",
+    85
   ),
   new Students(
     "Farel Darari Deksano",
     "farledeksano000@gmail.com",
-    "12-27-2000",
-    5
+    "11-15-1998",
+    90
   ),
   new Students(
     "Farel Darari Deksano",
     "farledeksano000@gmail.com",
-    "06-10-1955",
-    100
+    "01-30-2002",
+    78
   ),
 ];
 
@@ -88,6 +88,8 @@ function getNumber(arrStudents, typeCalc) {
 
 console.log(reportStudents(listStudent));
 
+// KAK BAGAS SOLUTION
+
 // ----------------------------------------------------------------
 
 // PRODUCT CLASS
@@ -109,12 +111,14 @@ class Transaction {
   constructor() {
     this.totalPrice = 0;
     this.listProduct = [];
+    this.qty = 0;
   }
 
-  addToCart(product) {
+  addToCart(product, qty) {
     if (this.listProduct.length == 0) {
       this.listProduct.push(product);
       this.totalPrice += product.productPrice;
+
       console.log(product.productName, "Product added to cart");
     } else {
       for (let i = 0; i < this.listProduct.length; i++) {
@@ -155,6 +159,63 @@ firstOrder.addToCart(listProduct[0]);
 firstOrder.addToCart(listProduct[2]);
 firstOrder.addToCart(listProduct[2]);
 
+console.log(firstOrder.listProduct);
+
 firstOrder.showTotal();
 firstOrder.checkout();
 console.log(firstOrder.listProduct);
+
+// KAK BAGAS SOLULTION
+
+class Product_2 {
+  constructor(productName, productPrice) {
+    this.productName = productName;
+    this.productPrice = productPrice;
+  }
+}
+
+class Transaction_2 {
+  constructor() {
+    this.listProduct = [];
+    this.qty = [];
+    this.totalPrice = 0;
+  }
+
+  addTocart(product, qty) {
+    this.listProduct.push(product);
+    this.qty.push(qty);
+    this.totalPrice += product.productPrice * qty;
+  }
+
+  showTotal() {
+    console.log(`Your total price is ${this.totalPrice}`);
+  }
+
+  checkout() {
+    let transactionData = [];
+    for (let i = 0; i < this.listProduct.length; i++) {
+      transactionData.push({
+        productName: this.listProduct[i].productName,
+        quantity: this.qty[i],
+        subTotal: this.listProduct[i].productPrice * this.qty[i],
+      });
+    }
+    transactionData.push({ totalPrice: this.totalPrice });
+    return transactionData;
+  }
+}
+
+const product_1 = new Product_2("Indomie", 3000);
+const product_2 = new Product_2("Es Podeng", 12000);
+const product_3 = new Product_2("Pizza Slice", 25000);
+
+console.log("product 1 : " + product_1);
+
+const transaction_1 = new Transaction_2();
+
+transaction_1.addTocart(product_1, 2);
+transaction_1.addTocart(product_2, 1);
+
+transaction_1.showTotal(); //
+
+console.log(transaction_1.checkout()); //
